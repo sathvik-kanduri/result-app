0e6aa9801831940b4914211d2d24d5a59039c329
sathvik kanduri <sathwikkanduri2000@gmail.com>
2023-07-05 13:44:34 +0530

    first commit

--- server.js
+++ server.js
@@ -1,42 +1,38 @@
-const express =require('express')
-const mongoose=require('mongoose')
-const userRoute=require('./routes/user.js');
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoute = require('./routes/user.js');
 const cookieParser = require('cookie-parser');
-const cors=require('cors');
-const app=express();
+const cors = require('cors');
+const app = express();
 
-
-mongoose.connect('mongodb+srv://sathvik:Sathvik%402000@cluster0.ovlrsyr.mongodb.net/Byoa')
-.then(()=>
-{
-    console.log('the database is connected')
-})
-.catch((err)=>{
-    console.log('the error is',err);
-})
+mongoose
+  .connect('mongodb+srv://sathvik:Sathvik%402000@cluster0.ovlrsyr.mongodb.net/Byoa')
+  .then(() => {
+    console.log('The database is connected');
+  })
+  .catch((err) => {
+    console.log('The error is', err);
+  });
 
 const corsOptions = {
-    origin: 'https://result-portal.onrender.com',
-  }
-  
-
+  origin: 'https://result-portal.onrender.com',
+  credentials: true, // Set credentials to true
+};
 
 app.use(cors(corsOptions));
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api/auth',userRoute);
+app.use('/api/auth', userRoute);
 
-app.get('/too',(req,res)=>{
-    res.send("cookie");
-})
+app.get('/too', (req, res) => {
+  res.send('cookie');
+});
 
-app.get('/app',(req,res)=>{
-    res.send("welcome to results app");
-})
+app.get('/app', (req, res) => {
+  res.send('welcome to results app');
+});
 
-
-
-app.listen(7000,()=>{
-    console.log('the port is 7000');
-})
+app.listen(7000, () => {
+  console.log('The port is 7000');
+});
